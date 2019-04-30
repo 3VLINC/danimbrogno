ae1c6f5ce2d1fd5b317dc06b95e2082c7b016908
Dan Imbrogno <dan@3vl.ca>
2019-04-30 05:44:08 +0000

    chore(release): update docs folder [skip ci]

---
+++ docs/precache-manifest.902b7929f586f93d4582064c17b6aa03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a12431fdac2ec791bb6baac59080e1d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "242e21db4ba93e231dab",
+    "url": "/static/css/main.71eac5dc.chunk.css"
+  },
+  {
+    "revision": "81b62f7dcc8ca98953bd",
+    "url": "/static/js/2.5d9c6547.chunk.js"
+  },
+  {
+    "revision": "242e21db4ba93e231dab",
+    "url": "/static/js/main.e33f8308.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
